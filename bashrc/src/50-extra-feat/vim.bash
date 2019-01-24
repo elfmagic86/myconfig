448449f1407aedbf8fe1982ac83e455d87ab4f3f
elfmagic86 <elfmagic86@gmail.com>
2019-01-24 09:11:18 +0900

    remove which no use

--- bashrc/src/50-extra-feat/vim.bash
+++ bashrc/src/50-extra-feat/vim.bash
@@ -1,19 +1,7 @@
 # vim
 # ---
 
-vim_open_nvim() {
-    if [ -z $1 ]; then
-        nvim
-    else
-        nvim "$@"
-    fi
-}
-
 vim_clean_backup() {
     rm -rf ~/.vim/backup
     mkdir -p ~/.vim/backup/tmp
 }
-
-
-alias nv='vim_open_nvim'
-alias vim='vim_open_nvim'
